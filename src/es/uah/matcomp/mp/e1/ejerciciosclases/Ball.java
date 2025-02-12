6c23d51b1e5b30b2e65b3ccaa7570b0809bd8694
DaniOrtegaR13 <d.ortegar@edu.uah.es>
2025-02-12 15:09:13 +0100

    Time and Ball tested and passed

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/Ball.java
@@ -0,0 +1,69 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+
+
+public class Ball {
+
+    private float x;
+    private float y;
+    private int radius;
+    private float Xdelta;
+    private float Ydelta;
+
+    public Ball (float x, float y, int radius, float Xdelta, float Ydelta) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.Xdelta = Xdelta;
+        this.Ydelta = Ydelta;
+
+    }
+
+    public float getX() {
+        return x;
+    }
+    public void setX(float x) {
+        this.x = x;
+    }
+    public float getY() {
+        return y;
+    }
+    public void setY(float y) {
+        this.y = y;
+    }
+    public int getRadius() {
+        return radius;
+    }
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    public float getXDelta() {
+        return Xdelta;
+    }
+    public void setXDelta(float deltaX) {
+        this.Xdelta = deltaX;
+    }
+    public float getYDelta() {
+        return Ydelta;
+    }
+    public void setYDelta(float deltaY) {
+        this.Ydelta = deltaY;
+    }
+
+    public void  move() {
+        //MOve one step
+        x += Xdelta;
+        y +=Ydelta;
+    }
+    public void reflectHorizontal() {
+        Xdelta= -Xdelta;
+    }
+    public void reflectVertical() {
+        Ydelta= -Ydelta;
+    }
+
+    public String toString() {
+        return "Ball[position =(" + x + "," + y + ") speed= (" + Xdelta + "," + Ydelta + ")]";
+    }
+
+}
